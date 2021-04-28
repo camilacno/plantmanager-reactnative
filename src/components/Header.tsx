81fb72d6ba41c0d892f399c34a3a4a148532fa0d
Camila Nepomuceno <camila.cno@gmail.com>
2021-04-27 21:25:40 -0300

    Header component [WIP]

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Camila</Text>
+      </View>
+      <Image
+        source={{ uri: 'https://avatars.githubusercontent.com/u/47459889?v=4' }}
+        style={{ width: 80, height: 80 }}
+        borderRadius={50}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    fontFamily: fonts.text,
+    color: colors.heading,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+  },
+});
