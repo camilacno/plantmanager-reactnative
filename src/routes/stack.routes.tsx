b46556c85cd5d6ba56e8b53775056693e84e4ee7
Camila Nepomuceno <camila.cno@gmail.com>
2021-05-10 12:42:42 -0300

    General config and data update

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 import colors from '../styles/colors';
 
@@ -23,6 +24,7 @@ const AppRoutes: React.FC = () => (
       component={UserIdentification}
     />
     <stackRoutes.Screen name='Confirmation' component={Confirmation} />
+    <stackRoutes.Screen name='PlantSelect' component={PlantSelect} />
   </stackRoutes.Navigator>
 );
 
