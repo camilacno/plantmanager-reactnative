6e477660f8c68f3843b01812ddd7e6dc43e0bb83
Camila Nepomuceno <camila.cno@gmail.com>
2021-05-16 18:54:21 -0300

    Refactor - ScrollView

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -79,48 +79,53 @@ export function PlantSave() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.plantInfo}>
-        <SvgFromUri uri={plant.photo} height={200} width={200} />
-        <Text style={styles.plantName}>{plant.name}</Text>
-        <Text style={styles.plantDetails}>{plant.about}</Text>
-      </View>
-
-      <View style={styles.controller}>
-        <View style={styles.tipContainer}>
-          <Image source={waterDrop} style={styles.tipImage} />
-          <Text style={styles.tipText}>{plant.water_tips}</Text>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.container}
+    >
+      <View style={styles.container}>
+        <View style={styles.plantInfo}>
+          <SvgFromUri uri={plant.photo} height={200} width={200} />
+          <Text style={styles.plantName}>{plant.name}</Text>
+          <Text style={styles.plantDetails}>{plant.about}</Text>
         </View>
 
-        <Text style={styles.alertLabel}>
-          Escolha o melhor horário para ser lembrado:
-        </Text>
-        {/* <Text style={styles.selectedTime}>{`Horário atual: ${format(
+        <View style={styles.controller}>
+          <View style={styles.tipContainer}>
+            <Image source={waterDrop} style={styles.tipImage} />
+            <Text style={styles.tipText}>{plant.water_tips}</Text>
+          </View>
+
+          <Text style={styles.alertLabel}>
+            Escolha o melhor horário para ser lembrado:
+          </Text>
+          {/* <Text style={styles.selectedTime}>{`Horário atual: ${format(
           selectedDateTime,
           'HH:mm'
         )}`}</Text> */}
 
-        {showDatePicker && (
-          <DateTimePicker
-            value={selectedDateTime}
-            mode='time'
-            display='default'
-            onChange={handleChangeTime}
-          />
-        )}
+          {showDatePicker && (
+            <DateTimePicker
+              value={selectedDateTime}
+              mode='time'
+              display='default'
+              onChange={handleChangeTime}
+            />
+          )}
 
-        {Platform.OS === 'android' && (
-          <TouchableOpacity
-            style={styles.dateTimePickerButton}
-            onPress={handleOpenDateTimePickerAndroid}
-          >
-            <Text style={styles.dateTimePickerText}>Selecionar Horário</Text>
-          </TouchableOpacity>
-        )}
+          {Platform.OS === 'android' && (
+            <TouchableOpacity
+              style={styles.dateTimePickerButton}
+              onPress={handleOpenDateTimePickerAndroid}
+            >
+              <Text style={styles.dateTimePickerText}>Selecionar Horário</Text>
+            </TouchableOpacity>
+          )}
 
-        <MainButton title='Cadastrar Planta' onPress={handleSave} />
+          <MainButton title='Cadastrar Planta' onPress={handleSave} />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
